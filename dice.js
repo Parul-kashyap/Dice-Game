85d34834939fd9c7bffce2e2adc3c4fa587acb52
Parul Kashyap <107707116+Parul-kashyap@users.noreply.github.com>
2024-08-11 16:06:05 +0530

    all files added

---
+++ dice.js
@@ -0,0 +1,26 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomImages1Source = 'images/' + 'dice' + randomNumber1 + '.png';
+var randomImages2Source = 'images/' + 'dice' + randomNumber2 + '.png';
+
+var image1 = document.querySelectorAll('img')[0].setAttribute('src',randomImages1Source);
+
+
+var image2 = document.querySelectorAll('img')[1].setAttribute('src',randomImages2Source);
+
+
+
+
+if (randomNumber1 > randomNumber2) {
+  document.querySelector('h1').innerHTML = '🏆Player1 wins';
+}
+
+else if (randomNumber2 > randomNumber1) {
+  document.querySelector('h1').innerHTML = 'Player2 wins🏆';
+}
+
+
+else  {
+  document.querySelector('h1').innerHTML = "🏆It's Draw🏆";
+}
